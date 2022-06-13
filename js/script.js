6113f46cd514f5e3cf3a71d6e6a134d8adc2d8ed
JohnLohata001 <37670263+JohnLohata001@users.noreply.github.com>
2022-06-14 00:30:59 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function previewImage(event) {
+
+    if (event.target.files.length > 0) {
+        const src = URL.createObjectURL(event.target.files[0]);
+        const preview = document.getElementById("file-preview");
+        preview.src = src;
+        preview.style.display = "block";
+    }
+    
+}
